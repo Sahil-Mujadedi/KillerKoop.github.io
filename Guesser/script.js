bd4742d3934146ac74c19e3973b1857722a40740
KillerKoop <sahil2006.com@gmail.com>
2020-08-05 22:06:03 -0600

    Will now tell player to not use letters or symbols when attempted

--- Guesser/script.js
+++ Guesser/script.js
@@ -1,3 +1,5 @@
+console.log("%c[ H e l l o ,  y o u  c h e a t e r . ]", "color: darkred;font-weight:bolder");
+
 // Store random number so we can reuse later for comparisons
 let numberToGuess = Math.floor((Math.random() * 100) + 1);
 let promptUser = document.querySelector('p');
@@ -7,15 +9,17 @@ let lives = 5;
 for(i = 0; i < lives; i++) {
     document.querySelector('#hearts').innerHTML += '&hearts;';
 }
+let userGuessBox = document.querySelector("#userGuess");
 
 document.addEventListener("keypress", function(event) {
     // Store user guess box in a variable and store the value of that 
     // in a different variable so we can reuse both later
-    let userGuessBox = document.querySelector("#userGuess");
+    
     let userGuess = userGuessBox.value;
 
     // Only run game logic if they press enter and they have typed something
     if(event.key === 'Enter' && userGuess.length > 0) {
+        promptUser.style.color = "black";
         if(userGuess > numberToGuess) {
             promptUser.innerHTML = 'Guess lower.';
             userGuessBox.style.borderColor = "red";
@@ -26,9 +30,13 @@ document.addEventListener("keypress", function(event) {
             userGuessBox.style.borderColor = "red";
             lives--;
         }
-        else {
+        else if(userGuess == numberToGuess) {
             promptUser.innerHTML = 'You win!';
             userGuessBox.style.borderColor = "lime";
+        }
+        else {
+            promptUser.innerHTML = 'Do not use letters or symbols.';
+            promptUser.style.color = "red";
         }
         // Makes heart bigger when at one life
         if(lives < 2) {
